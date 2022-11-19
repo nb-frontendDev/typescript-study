e3dda3fd4c08f20bc6dad444b342b90f12e153c7
渡邉将太 <shotawebcoding@gmail.com>
2022-11-19 17:44:14 +0900

    Update App.tsx

--- ts-study/src/App.tsx
+++ ts-study/src/App.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface NAME { 
+  first: string;
+  last?: string | null;
+}
+
+let nameObj: NAME = {
+  first: 'YAMADA',
+  last: null,
+}
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
