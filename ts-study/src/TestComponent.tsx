205108b90b2dad4d8a44ed6b2b6562558879c574
渡邉将太 <shotawebcoding@gmail.com>
2022-11-20 00:24:46 +0900

    Update TestComponent.tsx

--- ts-study/src/TestComponent.tsx
+++ ts-study/src/TestComponent.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props { 
     text: string;
 }
+interface UserData {
+    id: number;
+    name: string;
+}
 
 const TestComponent: React.FC<Props> = (props) => {
+
+    const [count, setCount] = useState<number | null>();
+    const [user, setUser] = useState<UserData>({
+        id: 1,
+        name:'yirii'
+    });
+    const [inputData, setInputData] = useState('');
+    
+    const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => { 
+        setInputData(e.target.value);
+    }
+
   return (
     <div>
-          <h1>{ props.text}</h1>
+          <h1>{props.text}</h1>
+          <h1>{count}</h1>
+          <div>
+              <input type="text" value={inputData} onChange={handleInputChange} />
+              <h1>{inputData}</h1>
+          </div>
     </div>
   )
 }
