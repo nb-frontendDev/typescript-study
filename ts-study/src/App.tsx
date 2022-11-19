f334aae3107f8d3c742f9249b2106d88fb9f0635
渡邉将太 <shotawebcoding@gmail.com>
2022-11-19 23:55:18 +0900

    Update App.tsx

--- ts-study/src/App.tsx
+++ ts-study/src/App.tsx
@@ -105,7 +105,61 @@ const comp1 = 'test';
 let comp2: string = comp1;
 
 let funcComp1 = (x: number) => {}
-let funcComp2 = (x: string) => {}
+let funcComp2 = (x: string) => { }
+
+//GENERICS
+interface GEN<T> { 
+  item: T;
+}
+const gen0: GEN<string> = {
+  item: 'hello'
+}
+const gen1: GEN<number> = {
+  item: 1
+}
+
+interface GEN1<T = string> { 
+  item: T;
+}
+const gen3: GEN1= {
+  item: 'string'
+}
+
+interface GEN2<T extends string | number> { 
+  item: T;
+}
+const gen4: GEN2<number>= {
+  item: 2,
+}
+
+//関数のジェネリクス
+function funcGEN<T> (props: T) { 
+  return {
+    item: props
+  }
+}
+const GEN6 = funcGEN<string | null>('test');
+
+function funcGEN1<T extends string | null> (props: T) { 
+  return {
+    value: props
+  }
+}
+const GEN8 = funcGEN1('hello');
+
+
+interface Props { 
+  price: number;
+}
+
+function GEN3<T extends Props>(props: T) { 
+  return {value: props.price}
+}
+const GEN9 = GEN3({
+  price: 1,
+})
+
+
 
 function App() {
   return (
