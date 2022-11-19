bb5dadd77782ca71ff55e833f0e6ee68cc304ffb
渡邉将太 <shotawebcoding@gmail.com>
2022-11-19 17:49:00 +0900

    Update App.tsx

--- ts-study/src/App.tsx
+++ ts-study/src/App.tsx
@@ -14,7 +14,26 @@ let nameObj: NAME = {
 
 const func1 = (x: number, y: number): number => {
   return x + y;
+}
 
+//Inter sction Type
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+// &(アンド)で型を繋げられる
+type USER = PROFILE & LOGIN;
+const userA: USER = {
+  age: 30,
+  city: 'Tokyo',
+  username: 'xxx',
+  password: 'yyy'
 }
 
 function App() {
